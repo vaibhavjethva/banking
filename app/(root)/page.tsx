f6bcdba21d4fadf83a6e97bc6787dccd5eb3bae2
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2024-05-12 17:33:24 +0530

    Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName:'Vaibhav'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently."
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
