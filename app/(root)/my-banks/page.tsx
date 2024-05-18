d3c73a7b47043a3f2c3d70ab3ab0d9595d82e565
Vaibhav Jethava <vaibhavjethava321@gmail.com>
2024-05-18 14:17:48 +0530

    Implemented all Layouts

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyBanks = () => {
+  return <div>MyBank</div>;
+};
+
+export default MyBanks;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,33 @@
-import HeaderBox from '@/components/HeaderBox'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import React from "react";
 
 const Home = () => {
-    const loggedIn = {firstName:'Vaibhav'};
+  const loggedIn = {
+    firstName: "Vaibhav",
+    lastName: "Jethava",
+    email: "vaibhavjethava321@gmail.com",
+  };
   return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName || 'Guest'}
-                    subtext="Access and manage your account and transactions efficiently."
-                />
-            </header>
-        </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 12350 }, { currentBalance: 50050 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
